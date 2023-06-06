97aa17d09fbbdbaa05c38bfbaa7b72dc8c986aaa
umutdunbay <umutdunbay@hotmail.com>
2023-06-06 21:59:21 +0300

    kodun çalışmayan kısımlarını ve mantık hatalarını düzelttim

--- issiz.py
+++ issiz.py
@@ -1,39 +1,35 @@
 from insan import insan
 
 class Issiz(insan):
-    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
         self.__statuler = {"beyaz yaka": 0, "mavi yaka": 0, "yönetici": 0}
-        self.set_statu(tecrube)
+        self.__statu = ""
+
+    def set_statuler(self, deger1, deger2, deger3):
+        self.__statuler = {"beyaz yaka": deger1, "mavi yaka": deger2, "yönetici": deger3}
 
     def get_statuler(self):
         return self.__statuler
 
-    def set_statu(self, tecrube):
-        self.__tecrube = tecrube
+    def set_statu(self, statu):
+        self.__statu = statu
 
+    def get_statu(self):
+        return self.__statu
 
 
     def statu_bul(self,):
-        mt = self.__statuler["mavi yaka"] * 0.20
-        bt = self.__statuler["beyaz yaka"] * 0.35
-        yt = self.__statuler["yönetici"] * 0.45
-        if mt > bt:
-            if mt > yt:
-                self.__tecrube = mt
-                print("En uygun statü mavi yakadır.")
-            else:
-                self.__tecrube = yt
-                print("En uygun statü yöneticidir.")
-        else:
-            if bt > yt:
-                self.__tecrube = bt
-                print("En uygun statü beyaz yakadır.")
-            else:
-                self.__tecrube = yt
-                print("En uygun statü yöneticidir")
+        mavi_yaka = (self.__statuler["mavi yaka"] * 0.20)
+        beyaz_yaka = (self.__statuler["beyaz yaka"] * 0.35)
+        yonetici = (self.__statuler["yönetici"] * 0.45)
+        self.__statuler = {"beyaz yaka": beyaz_yaka, "mavi yaka": mavi_yaka, "yönetici": yonetici}
+        self.__statu = max(self.__statuler, key=self.__statuler.get)
+
 
     def __str__(self,):
+        self.statu_bul()
         return f"Ad: {self.get_ad()}\n" \
                f"Soyad: {self.get_soyad()}\n" \
-               f"Önerilen Statü: {self.__tecrube}"
+               f"Önerilen Statü: {self.get_statu()}"
+

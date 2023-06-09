bdd2a85fc470eb9e148cccd345c95a4a072b27d2
umutdunbay <umutdunbay@hotmail.com>
2023-06-09 04:09:34 +0300

    zam_hakki işlemlerindeki hataları düzelttim

--- maviyaka.py
+++ maviyaka.py
@@ -11,31 +11,23 @@ class MaviYaka(Calisan):
     def set_yipranma_payi(self, yipranma_payi):
         try:
             yipranma_payi = float(yipranma_payi)
+            if yipranma_payi < 0:
+                print("Yıpranma payı negatif olamaz.")
+            self.__yipranma_payi = yipranma_payi
+
         except ValueError:
             print("Geçerli bir yıpranma payı değeri giriniz.")
-            return
-
-        if yipranma_payi < 0:
-            print("Yıpranma payı negatif olamaz.")
-            return
-
-        self.__yipranma_payi = yipranma_payi
 
     def zam_hakki(self):
         if self.get_tecrube() < 2:
-            return self.__yipranma_payi * 10
+            self.set_yeni_maas(self.get_maas() + self.get_maas() * self.get_yipranma_payi() /10)
         elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
-            return (self.get_maas() % self.get_tecrube() / 2) + (self.__yipranma_payi * 10)
+            self.set_yeni_maas(self.get_maas() +  self.get_maas() * (self.get_maas() % self.get_tecrube() / 2 + self.get_yipranma_payi() / 10))
         elif self.get_tecrube() > 4 and self.get_maas() < 25000:
-            return (self.get_maas() % self.get_tecrube() / 3) + (self.__yipranma_payi * 10)
-        else:
-            return 0
-
+            self.set_yeni_maas(self.get_maas() + self.get_maas() * (self.get_maas() % self.get_tecrube() / 3 + self.get_yipranma_payi() / 10))
     def __str__(self):
-        yeni_maas = Calisan.get_maas(self) + (Calisan.get_maas(self) * self.zam_hakki()) / 100
-        if yeni_maas == Calisan.get_maas(self):
-            yeni_maas = Calisan.get_maas(self)
+        self.zam_hakki()
         return f"Ad: {self.get_ad()}\n" \
                f"Soyad: {self.get_soyad()}\n" \
                f"Tecrübe: {self.get_tecrube()} ay\n" \
-               f"Yeni Maaş: {yeni_maas}"
+               f"Yeni Maaş: {self.get_yeni_maas()}"

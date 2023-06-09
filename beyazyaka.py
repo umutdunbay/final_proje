36a203c3b8f6161334ec8e3f348448965f05ffdd
umutdunbay <umutdunbay@hotmail.com>
2023-06-09 04:48:06 +0300

    beyazyaka.py dosyasına komut satırlarını da ekleyerek kodu tamamladık

--- beyazyaka.py
+++ beyazyaka.py
@@ -1,11 +1,17 @@
+# Calisan class'ından kalıtım yoluyla kullanacağımız BeyazYaka class'ı için
+# Öncelikle calisan.py dosyasından Calisan class'ını import ettik
 from calisan import Calisan
 
 
+# Calisan class'ına kalıtımla bağlı olacak olan BeyazYaka class'ını oluşturduk
 class BeyazYaka(Calisan):
+    # Uygun değişkenler için Initializer metot kullandık
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
+        # Zaten halihazırda Insan class'ında tanımlı değişkenler için super(). metodunu uyguladık
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__tesvik_primi = tesvik_primi
 
+    # Gerekli set/get fonksiyonları ile değişkenlerimi ulaşılabilir ve kullanılabilir hale getirdik
     def get_tesvik_primi(self):
         return self.__tesvik_primi
 
@@ -22,6 +28,8 @@ class BeyazYaka(Calisan):
             print("Lütfen geçerli bir teşvik primi değeri giriniz.")
             return
 
+    # Zam hakkı fonksiyonu ile çalışanın tecrübesine ve maaşına bağlı olarak belirli bir oranda
+    # MaviYaka'lardan farklı olarak direkt bir zam miktarı önerilmesini sağladık.
     def zam_hakki(self):
         if self.get_tecrube() < 2:
             self.set_yeni_maas(self.get_maas() + self.get_tesvik_primi())
@@ -30,6 +38,7 @@ class BeyazYaka(Calisan):
         elif self.get_tecrube() > 4 and self.get_maas() < 25000:
             self.set_yeni_maas((self.get_maas() + (self.get_maas() % self.get_tecrube()) * 4 + self.get_tesvik_primi()))
 
+    # Ekrana yazdırmak için kullanacağımız __str__ fonksiyonunu atadık
     def __str__(self):
         self.zam_hakki()
         return f"Ad: {self.get_ad()}\n" \

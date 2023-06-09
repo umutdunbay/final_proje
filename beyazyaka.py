bf445962f59bcdea69fd638155d573c142420454
umutdunbay <umutdunbay@hotmail.com>
2023-06-09 04:10:47 +0300

    zam miktari olarak almam gereken değeri zam oranı olarak aldığım için kodda düzeltmelerde bulundum

--- beyazyaka.py
+++ beyazyaka.py
@@ -12,31 +12,27 @@ class BeyazYaka(Calisan):
     def set_tesvik_primi(self, tesvik_primi):
         try:
             tesvik_primi = float(tesvik_primi)
+
+            if tesvik_primi < 0:
+                print("Teşvik primi değeri negatif olamaz.")
+                return
+
+            self.__tesvik_primi = tesvik_primi
         except ValueError:
             print("Lütfen geçerli bir teşvik primi değeri giriniz.")
             return
 
-        if tesvik_primi < 0:
-            print("Teşvik primi değeri negatif olamaz.")
-            return
-
-        self.__tesvik_primi = tesvik_primi
-
     def zam_hakki(self):
         if self.get_tecrube() < 2:
-            return self.__tesvik_primi
+            self.set_yeni_maas(self.get_maas() + self.get_tesvik_primi())
         elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
-            return (self.get_maas() % self.get_tecrube()) * 5 + self.__tesvik_primi
+            self.set_yeni_maas(self.get_maas() + ((self.get_maas() % self.get_tecrube()) * 5 + self.get_tesvik_primi()))
         elif self.get_tecrube() > 4 and self.get_maas() < 25000:
-            return (self.get_maas() % self.get_tecrube()) * 4 + self.__tesvik_primi
-        else:
-            return 0
+            self.set_yeni_maas((self.get_maas() + (self.get_maas() % self.get_tecrube()) * 4 + self.get_tesvik_primi()))
 
     def __str__(self):
-        yeni_maas = Calisan.get_maas(self) + self.zam_hakki()
-        if yeni_maas == Calisan.get_maas(self):
-            yeni_maas = Calisan.get_maas(self)
+        self.zam_hakki()
         return f"Ad: {self.get_ad()}\n" \
                f"Soyad: {self.get_soyad()}\n" \
                f"Tecrübe: {self.get_tecrube()} ay\n" \
-               f"Yeni Maaş: {yeni_maas}"
+               f"Yeni Maaş: {self.get_yeni_maas()}"
